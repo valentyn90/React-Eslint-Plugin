alexzherdev/eslint-plugin-react

Extract defaultProps detection code from default-props-match-prop-types

lib/util/Components.js
@@ -11,6 +11,7 @@ const variableUtil = require('./variable');
 const pragmaUtil = require('./pragma');
 const astUtil = require('./ast');
 const propTypes = require('./propTypes');
+const defaultProps = require('./defaultProps');
 
 function getId(node) {
   return node && node.range.join(':');
@@ -696,7 +697,9 @@ function componentRule(rule, context) {
   const ruleInstructions = rule(context, components, utils);
   const updatedRuleInstructions = util._extend({}, ruleInstructions);
   const propTypesInstructions = propTypes(context, components, utils);
-  const allKeys = new Set(Object.keys(detectionInstructions).concat(Object.keys(propTypesInstructions)));
+  const defaultPropsInstructions = defaultProps(context, components, utils);
+  const allKeys = new Set(Object.keys(detectionInstructions).concat(Object.keys(propTypesInstructions))
+    .concat(Object.keys(defaultPropsInstructions)));
   allKeys.forEach(instruction => {
     updatedRuleInstructions[instruction] = function(node) {
       if (instruction in detectionInstructions) {
@@ -704,6 +707,9 @@ function componentRule(rule, context) {
       }
       if (instruction in propTypesInstructions) {
         propTypesInstructions[instruction](node);
+      }
+      if (instruction in defaultPropsInstructions) {
+        defaultPropsInstructions[instruction](node);
       }
       return ruleInstructions[instruction] ? ruleInstructions[instruction](node) : void 0;
     };

valentyn90/React-Eslint-Plugin

lib/util/Components.js
@@ -9,9 +9,10 @@ const doctrine = require('doctrine');
 const variableUtil = require('./variable');
 const pragmaUtil = require('./pragma');
 const astUtil = require('./ast');
-const propTypes = require('./propTypes');
+const propTypesUtil = require('./propTypes');
 const jsxUtil = require('./jsx');
 const usedPropTypesUtil = require('./usedPropTypes');
+const defaultPropsUtil = require('./defaultProps');
 
 function getId(node) {
   return node && node.range.join(':');
@@ -708,12 +709,15 @@ function componentRule(rule, context) {
   // Update the provided rule instructions to add the component detection
   const ruleInstructions = rule(context, components, utils);
   const updatedRuleInstructions = util._extend({}, ruleInstructions);
-  const propTypesInstructions = propTypes(context, components, utils);
+  const propTypesInstructions = propTypesUtil(context, components, utils);
   const usedPropTypesInstructions = usedPropTypesUtil(context, components, utils);
+  const defaultPropsInstructions = defaultPropsUtil(context, components, utils);
   const allKeys = new Set(Object.keys(detectionInstructions).concat(
     Object.keys(propTypesInstructions),
-    Object.keys(usedPropTypesInstructions)
+    Object.keys(usedPropTypesInstructions),
+    Object.keys(defaultPropsInstructions)
   ));
+
   allKeys.forEach(instruction => {
     updatedRuleInstructions[instruction] = function(node) {
       if (instruction in detectionInstructions) {
@@ -724,6 +728,9 @@ function componentRule(rule, context) {
       }
       if (instruction in usedPropTypesInstructions) {
         usedPropTypesInstructions[instruction](node);
+      }
+      if (instruction in defaultPropsInstructions) {
+        defaultPropsInstructions[instruction](node);
       }
       return ruleInstructions[instruction] ? ruleInstructions[instruction](node) : void 0;
     };